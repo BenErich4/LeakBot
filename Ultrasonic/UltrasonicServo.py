421441b44eca88e3d730687a75c0b7fcb8c799c5
BenErich4 <40160102+BenErich4@users.noreply.github.com>
2018-09-02 00:01:23 +1000

    Update UltrasonicServo.py

--- Ultrasonic/UltrasonicServo.py
+++ Ultrasonic/UltrasonicServo.py
@@ -4,45 +4,36 @@
 
 import RPi.GPIO as GPIO
 from time import sleep
+from UltrasonicSensor.py import takeUltrasonicMeasurement
 
 # Defined Constants
-LEFT   = 175
-CENTRE = 90
-RIGHT  = 5
-DELAY  = 0.01
-
-pinNumber = 7                   # Assigned GPIO pin
+pinNumber = 7                   # Assigned GPIO pin to ultrasonic sensor's servo motor 
 GPIO.setwarnings(False)         # Disable console messages
 GPIO.setmode(GPIO.BOARD)        # Initialise GPIO pins
-
 
 def UltrasonicServoSetup():
   GPIO.setup(pinNumber, GPIO.OUT) # Set assigned GPIO pin as output
 
+# Set the angle of the ultra sonic sensor's servo motor
 def SetAngle(angle):
   duty = (angle / 18) + 2
-	GPIO.output(pinNumber, True)
-	pwm.ChangeDutyCycle(duty)
-	sleep(0.5)
-	GPIO.output(pinNumber, False)
-	pwm.ChangeDutyCycle(0)
+  GPIO.output(pinNumber, True)
+  pwm.ChangeDutyCycle(duty)
+  sleep(0.5)
+  GPIO.output(pinNumber, False)
+  pwm.ChangeDutyCycle(0)
 	
 # Check the robot's current line of sight
-def CheckLOS():
-  SetAngle(LEFT)   # Check Left
-  sleep(DELAY)
-  SetAngle(CENTRE) # Check Centre
-  sleep(DELAY)
-  SetAngle(RIGHT)  # Check Right
-  sleep(DELAY)
-  SetAngle(CENTRE) # Return to centre
+def fireSensor(position):
+  distanceMeasuredTemp = 0;
+  SetAngle(position); # Rotate Servo to position to take distance measurement
+  distanceMeasuredTemp = takeUltrasonicMeasurement();  
+  return (distanceMeasuredTemp);
 
-
-print("Start Motor Test")
-UltrasonicServoSetup()
-pwm=GPIO.PWM(pinNumber, 50)     # adds PWM functionality to GPIO pin (50 Hz)
-pwm.start(0)
-CheckLOS()
-pwm.stop()
-GPIO.cleanup()
-print("End Motor Test")
+def CheckLOS(positon):
+  UltrasonicServoSetup();
+  pwm=GPIO.PWM(pinNumber, 50); # adds PWM functionality to GPIO pin (50 Hz)
+  pwm.start(0);
+  fireSensor(position);
+  pwm.stop();
+  GPIO.cleanup();
