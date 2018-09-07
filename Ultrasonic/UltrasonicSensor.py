b33d6d6a9b643e4e6be8be4fcbfebd5ee248a36d
BenErich4 <40160102+BenErich4@users.noreply.github.com>
2018-09-07 15:57:18 +1000

    Update UltrasonicSensor.py

--- Ultrasonic/UltrasonicSensor.py
+++ Ultrasonic/UltrasonicSensor.py
@@ -1,7 +1,7 @@
 # This code is maintained by Benjamin Erich
 
 # This module interfaces an HC-SR04 (ultrasonic sensor) with a Raspberry Pi.
-# Add series external resistor to the ECHO pin as a safety precaution
+# @note: Add series external resistor to the ECHO pin as a safety precaution
 
 import RPi.GPIO as GPIO
 import time
@@ -11,8 +11,7 @@ TRIG = 7
 ECHO = 11
     
 def ultrasonicSensorSetup():
-    GPIO.setwarnings(False)    # Disable console messages
-    GPIO.setmode(GPIO.BOARD)   # Initialise GPIO pins 
+    #GPIO.setwarnings(False)    # Disable console messages
     GPIO.setup(ECHO, GPIO.IN)  # Sets the echo as an Input
     GPIO.setup(TRIG, GPIO.OUT) # Sets the trig as an Output
     GPIO.output(TRIG, 0)       # Set the trig pin LOW
@@ -20,7 +19,6 @@ def ultrasonicSensorSetup():
 
 def takeUltrasonicMeasurement():
     measuredDistanceTemp = 0;
-    print("Starting Measurement...")
 
     # Release a TRIGGER pulse from the ultrasonic sensor
     GPIO.output(TRIG, 1)
@@ -36,10 +34,10 @@ def takeUltrasonicMeasurement():
     stop = time.time()
 
     measuredDistanceTemp = round(((stop - start) * 17000), 3);
-    
-    # Display measurement and corresponding time
-    print("Distance:  " + str(measuredDistanceTemp) + " cm")
-    print("Time:      " + str(round((stop - start)*1000, 3)) + " ms")
 
-    GPIO.cleanup()
+    # @note: DEBUG ONLY
+    # Display measurement and corresponding time
+    # print("Distance:  " + str(measuredDistanceTemp) + " cm")
+    # print("Time:      " + str(round((stop - start)*1000, 3)) + " ms")
+    
     return measuredDistanceTemp;
